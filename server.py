9ceb52cb97606d6bef1750707478332a8ccf44db
sicongHeMacbookPro <1430728487@qq.com>
2017-04-17 20:13:57 +0800

    serverTest

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from itty import *
 
 @post('/')
 def post(request):
-	
 	return "username:" + request.POST.get("username","not specified")
+	
 @get('/')
 def get(request):
 	return "llbza server test"
