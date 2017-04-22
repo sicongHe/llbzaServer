ebf82acb89efc11cefd2117c0f74f9160a8b9b62
sicongHeMacbookPro <1430728487@qq.com>
2017-04-22 23:54:57 +0800

    4.22 update
    完成了部分显示与同步工作
    debug到昏迷

--- server.py
+++ server.py
@@ -2,11 +2,18 @@
 from itty import *
 import json
 online = []
-def getenemy(test):
-	if test == "pony":
-		return 'jb'
-	if test == 'jb':
+def getenemy(name):
+	if name == 'pony':
+		return 'chunge'
+	if name == 'chunge':
 		return 'pony'
+@post('/infoInit')
+def infoInit(request):
+	name = request.POST.get("name",'not specified')
+	jsonFile = open(name + '.json')
+	back = json.load(jsonFile)
+	print back
+	return Response(json.dumps(back), content_type='application/json')
 
 @post('/fightTest')
 def fightTest(request):
@@ -14,8 +21,6 @@ def fightTest(request):
 	enemy = getenemy(name)
 	back = {'enemyName': enemy}
 	return Response(json.dumps(back), content_type='application/json')
-
-
 
 @post('/')
 def post(request):
@@ -28,14 +33,15 @@ def post(request):
 		if (name == 'pony' and password == 'seed333333'):
 			online.append('pony')
 			return 'pony'
-		if (name == 'jb' and password == 'wocaonima'):
-			online.append('jb')
-			return 'jb'
+		if (name == 'chunge' and password == 'wocaonima'):
+			online.append('chunge')
+			return 'chunge'
 
 		else:
 			return 'fail'
 	else:
 		pass
+
 @get('/')
 def get(request):
 	a = ''
