e80a3e7643740457f370259b7f936aa331b8ab3b
sicongHeMacbookPro <1430728487@qq.com>
2017-04-22 17:04:51 +0800

    ui

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 #coding=utf-8 
 from itty import *
+import json
 online = []
+def getenemy(test):
+	if test == "pony":
+		return 'jb'
+	if test == 'jb':
+		return 'pony'
+
+@post('/fightTest')
+def fightTest(request):
+	name = request.POST.get("playerName",'not specified')
+	enemy = getenemy(name)
+	back = {'enemyName': enemy}
+	return Response(json.dumps(back), content_type='application/json')
+
+
+
 @post('/')
 def post(request):
-	flag = request.POST.get("flag","not specified");
+	flag = request.POST.get("flag","not specified")
 	if flag == 'login':
-		name = request.POST.get("username","not specified");
-		password = request.POST.get("password","not specified");
+		name = request.POST.get("username","not specified")
+		password = request.POST.get("password","not specified")
 		print name
 		print password
 		if (name == 'pony' and password == 'seed333333'):
@@ -20,7 +36,6 @@ def post(request):
 			return 'fail'
 	else:
 		pass
-	
 @get('/')
 def get(request):
 	a = ''
